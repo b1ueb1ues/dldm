e2c5083d264349a7a5f389e9992c523ba46d5066
b1ueb1ues <bblues@live.cn>
2019-12-13 01:44:57 +0800

    finish frame

--- frame.py
+++ frame.py
@@ -3,26 +3,41 @@ ACTIONSDIR = 'out/mono/actions'
 ANIMATION = 'out/anim/meshes/characters/motion/animationclips'
 OVERRIDE = 'out/anim/resources/characters/motion'
 SKILLDATA = 'out/mono/master/skilldata.asset'
+CHARADATA = 'out/mono/master/charadata.asset'
 TEXTLABEL = 'out/mono/master/textlabel.asset'
-#TEXTLABEL = 'TextLabel.txt'
+
+TEXTLABEL = 'TextLabel.txt'
 
 ###############################################################################
 import os
 import re
 
 aidframe = {}
+aidlabel = {}
 def aid2frame(fname):
-    data = open(fname).read()
-    r = re.findall(r'commandType = 15\n.*float _seconds = (.+)\n.*float _speed = (.*)\n', data)
-    if len(r) != 1:
-        return
-    _seconds, _speed = r[0]
+    global aidframe, aidlabel
     aid = re.findall(r'playeraction_(\d+).*', fname)
-    aid = int(aid[0])
-    duration = float(_seconds)*float(_speed)
-    frame = int(duration*60+0.01)
-    #print(aid, frame)
-    aidframe[aid] = frame
+    if len(aid) == 1:
+        aid = int(aid[0])
+    else:
+        return
+    data = open(fname).read()
+    pmds = re.findall(r'PartsMotionData _data\n.*?\n\n', data, re.DOTALL)
+    label = []
+    for i in pmds:
+        _seconds = re.findall(r'float _seconds = (.*)\n', i)[0]
+        _speed = re.findall(r'float _speed = (.*)\n', i)[0]
+        ms = re.findall(r'string _motionState = "(.*)"\n', i)[0]
+        label.append((ms, _seconds, _speed))
+    r = re.findall(r'commandType = 15\n.*float _seconds = (.+)\n.*float _speed = (.*)\n', data)
+    if len(label) >= 1:
+        aidlabel[aid] = label
+    if len(r) >= 1:
+        _seconds, _speed = r[0]
+        #duration = float(_seconds)*float(_speed)  
+        duration = float(_seconds)
+        frame = int(duration*60+0.01)
+        aidframe[aid] = frame
 
 sa = {}
 sae = {}
@@ -49,8 +64,9 @@ def sid2aid():
 
 idname = {}
 idcomment = {}
+idid = {}
 def cid2name():
-    global idchara, idcomment
+    global idchara, idcomment, idid
     data = open(TEXTLABEL).read()
     tmp = re.findall(r'CHARA_NAME_(\d+)"\n.*_Text = "(.*)"', data)
     for i in tmp:
@@ -59,6 +75,13 @@ def cid2name():
     for i in tmp:
         idcomment[i[0]] = i[1]
         idname[i[0]] = i[1]
+    data = open(CHARADATA).read()
+    cdes = re.findall(r'CharaDataElement data.*?\n.*?\[\d*\]\n', data, re.DOTALL)
+    for i in cdes:
+        cid = re.findall(r'int _Id = (\d+)\n', i)[0]
+        bid = re.findall(r'int _BaseId = (\d+)\n', i)[0]
+        vid = re.findall(r'int _VariationId = (\d+)\n', i)[0]
+        idid[cid] = (int(bid), int(vid))
 
 labelframe = {}
 def anim2frame(fname):
@@ -97,9 +120,6 @@ def main():
     for root, dirs, files in os.walk(SRC, topdown=False):
         for f in files:
             anim2frame(root+'/'+f)
-    global labelframe
-    print(labelframe)
-    exit()
 
     for cid in idname:
         name = idname[cid]
@@ -111,18 +131,42 @@ def main():
         f2 = []
         for i in aid1:
             if i:
-                if i in aidframe:
-                    f1.append(aidframe[i])
-                else:
-                    f1.append(-1)
+                f1.append(_do(cid, i))
         for i in aid2:
             if i:
-                if i in aidframe:
-                    f2.append(aidframe[i])
-                else:
-                    f2.append(-1)
-        print('%s,%s,%s,"%s %s %s"'%(name, f1, f2, cid, aid1, aid2))
+                f2.append(_do(cid, i))
+        print('"%s","%s","%s","%s %s %s"'%(name, f1, f2, cid, aid1, aid2))
 
+def _do(cid, aid):
+    global aidframe, idid, labelframe
+    if aid in aidframe:
+        canceltime = aidframe[aid]
+    else:
+        canceltime = -1
+    if aid in aidlabel:
+        labels = aidlabel[aid]
+    else:
+        labels = [['','0','0']]
+    label = labels[-1][0]
+    #startframe = float(labels[-1][1]) * float(labels[-1][2])
+    startframe = float(labels[-1][1])
+    startframe = int(startframe*60+0.01)
+    bvid = idid[cid]
+    bvid = '%d%02d'%(bvid[0], bvid[1])
+    labeltime = -1
+    if label in labelframe:
+        labeltime = labelframe[label]
+    elif 'skill_unique_' in label:
+        lid = label.replace('skill_unique_', '')
+        aname = 'SKL_'+lid+'_'+bvid
+        for l in labelframe:
+            if aname in l:
+                labeltime = labelframe[l] + startframe
+                break
+    if canceltime >= 0:
+        return canceltime
+    else:
+        return labeltime
 
 
 
