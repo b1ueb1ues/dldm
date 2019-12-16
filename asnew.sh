f8f56a62cb40134a7aa7909488b5df1cb7401813
b1ueb1ues <bblues@live.cn>
2019-12-16 14:36:51 +0800

    update

--- asnew.sh
+++ asnew.sh
@@ -1,5 +1,6 @@
 rm /data/local/tmp/as.bak
 mv /data/local/tmp/as.tar /data/local/tmp/as.bak
-echo '--start--------------'
 echo 'cd /data/data/com.nintendo.zaga/files/assets; tar cf /data/local/tmp/as.tar `ls -al|grep $(date +"%m-%d")|cut -d " " -f12`'
+echo '--start--------------'
+cd /data/data/com.nintendo.zaga/files/assets; tar cf /data/local/tmp/as.tar `ls -al|grep $(date +"%m-%d")|cut -d " " -f12`
 echo '--end----------------'
