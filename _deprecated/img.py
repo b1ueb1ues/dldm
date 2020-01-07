0974c35d3e8c96b71eaa6337e56ea5896112ef98
b1ueb1ues <bblues@live.cn>
2020-01-07 10:06:46 +0800

    update

---
+++ _deprecated/img.py
@@ -0,0 +1,10 @@
+import x
+
+x.INDIR = 'assets'
+x.OUTDIR = 'out/img'
+x.TYPE_FILTER = ['Texture2D', 'Sprite','Material','MonoBehaviour']
+#x.PATH_FILTER = []
+x.PREFIX = 'assets/_gluonresources/'
+x.CLEAN = False
+x.RENAME = True
+x.main()
