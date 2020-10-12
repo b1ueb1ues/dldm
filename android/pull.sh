910cb6fef8a8098b329e2f3a6de874a5995becec
b1ueb1ues <bblues@live.cn>
2020-10-12 23:27:46 +0800

    update

--- android/pull.sh
+++ android/pull.sh
@@ -1,4 +1,10 @@
-rm -rf assets
+#!/bin/sh
+dir=`dirname $0`
+echo '-- clean ----'
+cd $dir/../
 rm as.tar
+rm -rf assets
+echo '-- pull ----'
 adb pull /data/local/tmp/as.tar
+echo '-- extract ----'
 tar xf as.tar
